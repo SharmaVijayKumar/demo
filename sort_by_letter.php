66d6dd5756504a8e83ca5c8ec5ed6935ff8583f8
freelancerpush <freelancerpush@gmail.com>
2016-12-08 12:26:43 +0530

    My Project Committed by me.

---
+++ sort_by_letter.php
@@ -0,0 +1,17 @@
+<?php
+
+$strings = array('Foo', 'Moo', 'Xuux', 'Me', 'Blah', 'Ma');
+
+echo 'before- <pre>';print_r($strings);
+$letter = 'M';
+
+usort($strings, function($a, $b) use($letter) {
+    if($a[0] != $b[0]) {
+        if($a[0] == $letter) return -1;
+        if($b[0] == $letter) return +1;
+    }
+    return strcmp($a, $b);  
+});
+
+echo 'after- <pre>';print_r($strings);
+?>
